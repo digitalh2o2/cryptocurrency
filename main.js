cda4df8d56a6465b6637b91855e79c9e5599d57c
Saul <digitalh2o@gmail.com>
2017-10-27 16:15:48 -0500

    Pulling API data and accessing DOM. Will continue to add additional info to DOM from API

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-// alert("hi")
+// Query Selectors //
+
+const columns = document.getElementsByClassName('col-1-5');
+
+console.log(columns);
+
+const pricePromise = fetch("https://api.coinmarketcap.com/v1/ticker/?limit=5")
+
+pricePromise
+  .then(function(data) {
+    data.json()
+    .then(function(data){
+      for(let i = 0; i < columns.length; i++) {
+        columns[i].innerHTML = `<h1>${data[i].name}</h1>`
+      }
+      console.log(data[0].name)
+      console.log(data);
+    })
+    .catch(function(err){
+      console.log(err);
+    })
+  })
