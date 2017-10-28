52aa59738ec931b025354834cbd4449a21a671c7
Saul <digitalh2o@gmail.com>
2017-10-27 19:21:22 -0500

    Completed API pull and DOM updates. CSS Media queries next

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
 // Query Selectors //
 
 const columns = document.getElementsByClassName('col-1-5');
+const button = document.querySelector('button')
+button.addEventListener('click', getPromise);
 
-console.log(columns);
+// Promise Function //
 
-const pricePromise = fetch("https://api.coinmarketcap.com/v1/ticker/?limit=5")
+function getPromise() {
+  const pricePromise = fetch("https://api.coinmarketcap.com/v1/ticker/?limit=5");
+  pricePromise
+    .then(function(data) {
+      data.json()
+      .then(function(data){
+          for(let i = 0; i < columns.length; i++) {
+            columns[i].innerHTML =
+              `<h1>${data[i].name}</h1>
 
-pricePromise
-  .then(function(data) {
-    data.json()
-    .then(function(data){
-      for(let i = 0; i < columns.length; i++) {
-        columns[i].innerHTML = `<h1>${data[i].name}</h1>`
-      }
-      console.log(data[0].name)
-      console.log(data);
+               <h3>Rank: ${data[i].rank}</h1>
+               <h4>Current Price: $${data[i].price_usd}</h4>
+               <h4>Percent Change 24h</h4>
+              ${data[i].percent_change_24h > 0 ?
+                `<h5 class="positive">+${data[i].percent_change_24h}%</h5>` : `<h5 class="negative">${data[i].percent_change_24h}%</h5>`
+              }
+              `
+        }
+      })
+      .catch(function(err){
+        console.log(err);
+      })
     })
-    .catch(function(err){
-      console.log(err);
-    })
-  })
+}
